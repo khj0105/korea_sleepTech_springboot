5ebd6b3f1baa0f1fb22f41f6f3786842bdfa7225
khj0105 <khj0001052@gmail.com>
2025-05-23 15:13:29 +0900

    20250523 Spring Boot

---
+++ src/main/java/com/example/korea_sleepTech_springboot/service/PostDataService.java
@@ -0,0 +1,15 @@
+package com.example.korea_sleepTech_springboot.service;
+
+import com.example.korea_sleepTech_springboot.dto.file.PostRequestDto;
+import com.example.korea_sleepTech_springboot.dto.file.PostResponseDto;
+import com.example.korea_sleepTech_springboot.dto.response.ResponseDto;
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+@Service
+public interface PostDataService {
+    ResponseDto<PostResponseDto> createPost(@Valid PostRequestDto dto, MultipartFile file) throws IOException;
+}
